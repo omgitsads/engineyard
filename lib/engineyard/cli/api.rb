d135a21fc6f1ca7931bcdf1848deed04418fc079
Martin Emde <martin.emde@gmail.com>
2011-12-11 23:03:56 -0800

    Don't inherit from apiclient in the cli api wrapper.

--- lib/engineyard/cli/api.rb
+++ lib/engineyard/cli/api.rb
@@ -3,18 +3,20 @@ require 'engineyard-api-client'
 
 module EY
   class CLI
-    class API < EY::APIClient
+    class API
+      attr_reader :token
 
       def initialize(token = nil)
         @token = token
         @token ||= EY::EYRC.load.api_token
         @token ||= self.class.fetch_token
         raise EY::Error, "Sorry, we couldn't get your API token." unless @token
+        @api = EY::APIClient.new(@token)
       end
 
-      def request(*)
+      def request(*args)
         begin
-          super
+          @api.request(*args)
         rescue EY::APIClient::InvalidCredentials
           EY.ui.warn "Credentials rejected; please authenticate again."
           refresh
@@ -31,10 +33,21 @@ module EY
         begin
           email    = EY.ui.ask("Email: ")
           password = EY.ui.ask("Password: ", true)
-          super(email, password)
+          token = EY::APIClient.authenticate(email, password)
+          EY::EYRC.load.api_token = token
         rescue EY::APIClient::InvalidCredentials
           EY.ui.warn "Invalid username or password; please try again."
           retry
+        end
+      end
+
+      protected
+
+      def method_missing(meth, *args, &block)
+        if @api.respond_to?(:meth)
+          @api.send(meth, *args, &block)
+        else
+          super
         end
       end
 
